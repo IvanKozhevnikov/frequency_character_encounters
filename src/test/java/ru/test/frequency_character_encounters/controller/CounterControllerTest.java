d76556008043aa269edb669b39643871e0462e33
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-09-18 22:25:17 +0300

    Test

--- src/test/java/ru/test/frequency_character_encounters/controller/CounterControllerTest.java
+++ src/test/java/ru/test/frequency_character_encounters/controller/CounterControllerTest.java
@@ -1,21 +1,17 @@
 package ru.test.frequency_character_encounters.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import ru.test.frequency_character_encounters.Main;
-import ru.test.frequency_character_encounters.model.Input;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc

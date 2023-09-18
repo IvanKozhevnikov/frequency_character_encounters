e290db9fa49404ed8a5eb9a099c6d9df84b8e128
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-09-18 19:03:33 +0300

    Test

--- src/main/java/ru/test/frequency_character_encounters/model/Input.java
+++ src/main/java/ru/test/frequency_character_encounters/model/Input.java
@@ -1,8 +1,10 @@
 package ru.test.frequency_character_encounters.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Input {
     private String symbols;
 }
--- src/test/java/ru/test/frequency_character_encounters/controller/CounterControllerTest.java
+++ src/test/java/ru/test/frequency_character_encounters/controller/CounterControllerTest.java
@@ -2,24 +2,21 @@ package ru.test.frequency_character_encounters.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ru.test.frequency_character_encounters.Main;
 import ru.test.frequency_character_encounters.model.Input;
-import ru.test.frequency_character_encounters.service.SimpleCountService;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringBootTest
+@SpringBootTest(classes = Main.class)
 @AutoConfigureMockMvc
 class CounterControllerTest {
 
@@ -29,29 +26,37 @@ class CounterControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @MockBean
-    private SimpleCountService countService;
+    private Input input;
 
     @Test
-    @WithMockUser
-    public void shouldReturnDefaultMessage() throws Exception {
-        this.mockMvc.perform(post("/count/out")
-                .param("aaaaabccccyyyyyyyy", "Куплю ладу-гранту. Дорого."))
-                .andDo(print())
-                .andExpect(status().is3xxRedirection());
-        ArgumentCaptor<Input> argument = ArgumentCaptor.forClass(Input.class);
-        verify(countService).createAndSortedMap(argument.capture());
-        assertThat(argument.getValue().getSymbols()).isEqualTo("Куплю ладу-гранту. Дорого.");
+    public void createEmployeeAPI() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders
+                .post("/count/out")
+                .content(asJsonString(new Input("aaaaabccccyyyyyyyy")))
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.employeeId").exists());
     }
 
-    @Test
-    public void givenStudents_whenGetAllStudents_thenListOfStudents() throws Exception {
-        this.mockMvc.perform(MockMvcRequestBuilders.post("/count/out")
-                .contentType("application/json").content("aaaaabccccyyyyyyyy")
+    public static String asJsonString(final Object obj) {
+        try {
+            final ObjectMapper mapper = new ObjectMapper();
+            final String jsonContent = mapper.writeValueAsString(obj);
+            return jsonContent;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+//    @Test
+//    public void givenStudents_whenGetAllStudents_thenListOfStudents() throws Exception {
+//        this.mockMvc.perform(MockMvcRequestBuilders.post("/count/out")
+//                .contentType("application/json").content("aaaaabccccyyyyyyyy")
+//
+//                .andExpect(status().is3xxRedirection());
+//        ArgumentCaptor<Input> argument = ArgumentCaptor.forClass(Input.class);
+//        verify(countService).createAndSortedMap(argument.capture());
+//        assertThat(argument.getValue().getSymbols()).isEqualTo("aaaaabccccyyyyyyyy");
+//    }
 
-                .andExpect(status().is3xxRedirection());
-        ArgumentCaptor<Input> argument = ArgumentCaptor.forClass(Input.class);
-        verify(countService).createAndSortedMap(argument.capture());
-        assertThat(argument.getValue().getSymbols()).isEqualTo("aaaaabccccyyyyyyyy");
-    }
-    }
+}

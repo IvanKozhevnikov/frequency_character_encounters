0c52f53f87da4702b14964c17aa1921dd2ca8b8f
Ivan_Kozhevnikov <IvanKozhevnikof@yandex.ru>
2023-09-18 22:55:56 +0300

    Test

--- src/main/java/ru/test/frequency_character_encounters/model/Input.java
+++ src/main/java/ru/test/frequency_character_encounters/model/Input.java
@@ -1,12 +1,12 @@
 package ru.test.frequency_character_encounters.model;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
 
 @Data
 public class Input {
-    @NotNull(message = "Symbols must be non null")
+
+    @NotBlank(message = "The string must not be empty")
     private String symbols;
 }
